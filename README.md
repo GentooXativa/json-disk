b69692f5f3de56345a4d1e1bc726a9d115a1a0d4
Jose Vicente Giner <jose.giner@ara-tech.es>
2017-09-03 09:49:57 +0200

    Added help argument Added argument to use /etc/mtab instead /etc/fstab

--- README.md
+++ README.md
@@ -6,48 +6,48 @@ A simple C application to get the information about mountpoints and free space w
 [
   {
     "mountpoint": "/",
-    "total": 1093038080,
-    "free": 20506800128,
+    "free": 1092571136,
+    "total": 20506800128,
     "type": "ext4",
     "fsname": "/dev/md2",
     "fsopts": "errors=remount-ro,relatime"
   },
   {
     "mountpoint": "/srv",
-    "total": 304944160768,
-    "free": 3916700098560,
+    "free": 304944160768,
+    "total": 3916700098560,
     "type": "ext4",
     "fsname": "/dev/md3",
     "fsopts": "defaults,relatime"
   },
   {
     "mountpoint": "swap",
-    "total": 304944160768,
-    "free": 3916700098560,
+    "free": 304944160768,
+    "total": 3916700098560,
     "type": "swap",
     "fsname": "/dev/sda4",
     "fsopts": "defaults"
   },
   {
     "mountpoint": "swap",
-    "total": 304944160768,
-    "free": 3916700098560,
+    "free": 304944160768,
+    "total": 3916700098560,
     "type": "swap",
     "fsname": "/dev/sdb4",
     "fsopts": "defaults"
   },
   {
     "mountpoint": "swap",
-    "total": 304944160768,
-    "free": 3916700098560,
+    "free": 304944160768,
+    "total": 3916700098560,
     "type": "swap",
     "fsname": "/dev/sdc4",
     "fsopts": "defaults"
   },
   {
     "mountpoint": "/dev/shm",
-    "total": 8401739776,
     "free": 8401739776,
+    "total": 8401739776,
     "type": "tmpfs",
     "fsname": "tmpfs",
     "fsopts": "defaults"
--- main.c
+++ main.c
@@ -1,44 +1,72 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <mntent.h>
 
 #include <sys/statvfs.h>
 #include <sys/types.h>
 #include <sys/vfs.h>
 
+#define APP_VERSION   "0.0.3"
+#define REPO_URL      "https://github.com/GentooXativa/json-disk"
+
 typedef enum { false, true } bool;
+
+int extractInfo( const char *filename ){
+  struct mntent *ent;
+  struct statfs vfs;
+  FILE *aFile;
+  bool first = true;
+
+  aFile = setmntent(filename, "r");
+  if (aFile == NULL) {
+    perror("setmntent");
+    exit(1);
+  }
+
+  printf("[");
+  while (NULL != (ent = getmntent(aFile))) {
+    statfs(ent->mnt_dir, &vfs);      
+    
+    if(vfs.f_blocks > 0 ){
+      if( first != true ){
+        printf(",");
+      }else{
+        first = false;
+      }
+
+      printf("{ \"mountpoint\": \"%s\", \"free\": %ld, \"total\": %ld, \"type\": \"%s\", \"fsname\": \"%s\", \"fsopts\": \"%s\" }",
+        ent->mnt_dir, ( vfs.f_bsize * vfs.f_bfree ), ( vfs.f_bsize * vfs.f_blocks ), ent->mnt_type, ent->mnt_fsname, ent->mnt_opts );
+    }
+  }
+
+  printf("]\n");
+  endmntent(aFile);
+  return 0;
+}
 
 int main (int argc, char *argv[])
 {
-    struct mntent *ent;
-    struct statfs vfs;
-    FILE *aFile;
-    bool first = true;
+  int i;
+  char *fname = "/etc/fstab";
 
-    aFile = setmntent("/etc/fstab", "r");
-    if (aFile == NULL) {
-      perror("setmntent");
-      exit(1);
-    }
+   /* iterate over all arguments */
+   for (i = 0; i < argc; i++) {
+    if (strcmp("-h", argv[i]) == 0) {
+      printf("JSON-DISK v%s\n%s\n\n", APP_VERSION, REPO_URL );
+      printf("Usage: json_disk [-h] [-m]\n");
+      printf("\t-h\tShow this help\n");
+      printf("\t-m\tUse /etc/mtab instead of /etc/fstab\n");
+      return 0;
+   }
+   
+   if (strcmp("-m", argv[i]) == 0) {
+    fname = "/etc/mtab";
+    continue;
+ }
+ 
+}  
+  return extractInfo(fname);
 
-    printf("[");
-    while (NULL != (ent = getmntent(aFile))) {
-      statfs(ent->mnt_dir, &vfs);      
-      
-      if(vfs.f_blocks > 0 ){
-        if( first != true ){
-          printf(",");
-        }else{
-          first = false;
-        }
-
-        printf("{ \"mountpoint\": \"%s\", \"free\": %ld, \"total\": %ld, \"type\": \"%s\", \"fsname\": \"%s\", \"fsopts\": \"%s\" }",
-          ent->mnt_dir, ( vfs.f_bsize * vfs.f_bfree ), ( vfs.f_bsize * vfs.f_blocks ), ent->mnt_type, ent->mnt_fsname, ent->mnt_opts );
-      }
-    }
-
-    printf("]\n");
-    endmntent(aFile);
-    return 0;
 }
